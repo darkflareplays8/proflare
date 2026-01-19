643dfc24e75c1cc1b0cae3de8e06b1b982387db7
DarkFlare <saimuralibalmuri@gmail.com>
2026-01-19 16:46:31 +0000

    Create all.js

---
+++ commands/all.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = [
+  {
+    data: new SlashCommandBuilder()
+      .setName('autototem')
+      .setDescription('AutoTotem+'),
+    async execute(interaction) {
+      await interaction.reply('🔥 **AutoTotem+**: https://placehold.com');
+    }
+  },
+  {
+    data: new SlashCommandBuilder()
+      .setName('autorocket')
+      .setDescription('AutoRocket+'),
+    async execute(interaction) {
+      await interaction.reply('🚀 **AutoRocket+**: https://placehold.co.');
+    }
+  },
+  {
+    data: new SlashCommandBuilder()
+      .setName('performance-eternal')
+      .setDescription('Performance Eternal'),
+    async execute(interaction).reply('Will do this later');
+    }
+  }
+];
